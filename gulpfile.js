ac045e4e6489f124a3b59ffa3257d924a9896cb5
Martin Wragg <martin.wragg@ge.com>
2016-11-29 17:03:19 -0800

    try again

--- gulpfile.js
+++ gulpfile.js
@@ -224,7 +224,7 @@ gulp.task('generate-service-worker', function(callback) {
   var rootDir = '.';
 
   swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-    staticFileGlobs: [rootDir + '/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    staticFileGlobs: [rootDir + '/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}', '!' + rootDir + '/_*.{js,html,css}'],
     stripPrefix: rootDir
   }, callback);
 });
